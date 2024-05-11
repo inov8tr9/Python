e47dd489156e75017e01a33284faeac519010dac
inov8tr9 <32400414+inov8tr9@users.noreply.github.com>
2024-05-11 09:37:29 -0400

    add print lines

--- Python.org/4-1-6-12 - SUMMARY Exercises Tuples and Dicts.py
+++ Python.org/4-1-6-12 - SUMMARY Exercises Tuples and Dicts.py
@@ -2,29 +2,33 @@
 # What happens when you attempt to run the following snippet?
 myTup = (1, 2, 3)
 print(myTup[2])  # you get 3 as its 3rd index elem
+print()
 
 # Exercise 2
 # What is the output of the following snippet?
 tup = 1, 2, 3
 a, b, c = tup
-print(a * b * c)  # you get 6 because tuples have already been converted to values = simple math now
+print(a * b * c)  # you get 'SIX' because tuples have already been converted to values = simple math now
+print()
 
 # Exercise 3
 # Complete the code to correctly use the count() method to find the number of duplicates of 2 in the following tuple.
 tup = 1, 2, 3, 2, 4, 5, 6, 2, 7, 2, 8, 9
 duplicates = tup.count(2)  # your code
-print(duplicates)    # outputs: 4
+print(duplicates)  # outputs: 4
+print()
 
 # Exercise 4
 # Write a program that will "glue" the two dictionaries (d1 and d2) together and create a new one (d3).
-d1 = {'Adam Smith':'A', 'Judy Paxton':'B+'}
-d2 = {'Mary Louis':'A', 'Patrick White':'C'}
+d1 = {'Adam Smith': 'A', 'Judy Paxton': 'B+'}
+d2 = {'Mary Louis': 'A', 'Patrick White': 'C'}
 d3 = {}
 
 for item in (d1, d2):
-     # d3.append()  # your code BUT WRONG
-     d3.update(item)  # correct way, they gave you 'item' already !!
+    # d3.append()  # your code BUT WRONG
+    d3.update(item)  # correct way, they gave you 'item' already !!
 print(d3)
+print()
 
 # Exercise 5
 # Write a program that will convert the l list to a tuple.
@@ -34,6 +38,7 @@ print(l)
 t = tuple(l)  # your code = correct
 print(t)
 print(type(t))
+print()
 
 # Exercise 6
 # Write a program that will convert the colors tuple to a dictionary.
@@ -42,24 +47,26 @@ colDict = dict(colors)  # your code, dict is OK do NOT have to type out 'diction
 
 print(colDict)
 print(type(colDict))
+print()
 
 # Exercise 7
 # What will happen when you run the following code?
-myDict = {"A":1, "B":2}
+myDict = {"A": 1, "B": 2}
 copyMyDict = myDict.copy()  # made a copy of it
 myDict.clear()  # cleared out the original one
 
 print(copyMyDict)  # {'A': 1, 'B': 2}  # this is answer.... don't be faked out.
 print(myDict)  # this is empty
+print()
 
 # Exercise 8
 # What is the output of the following program?
 colors = {
-    "white" : (255, 255, 255),  # this looks like properly constructed Dict
-    "grey"  : (128, 128, 128),
-    "red"   : (255, 0, 0),
-    "green" : (0, 128, 0)
-    }
+    "white": (255, 255, 255),  # this looks like properly constructed Dict
+    "grey": (128, 128, 128),
+    "red": (255, 0, 0),
+    "green": (0, 128, 0)
+}
 
 for col, rgb in colors.items():
     print(col, "->", rgb)  # you get a chart... it is accurate.
@@ -71,7 +78,7 @@ for i in range(1, 11):
 print(newListy)
 
 for k in range(30, 36):
-     newListy.append(k)
+    newListy.append(k)
 print(newListy)
 
 newListy.insert(8, 99)
